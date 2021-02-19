b36024f133b86d945780560ac9ea6236f3f102b1
Timofeimih <timofeimih@gmail.com>
2021-02-19 18:19:04 +0200

    delete data

--- data-raw/trainandtest.R
+++
@@ -1,9 +0,0 @@
-
-
-trainfile <- read.table("inst/extdata/albretch_train.csv", sep=",",header=TRUE, stringsAsFactors=FALSE, dec = ".") #read data
-testfile <- read.table("inst/extdata/albretch_test.csv", sep=",",header=TRUE, stringsAsFactors=FALSE, dec = ".") #read data
-
-usethis::use_data(trainfile, overwrite = TRUE)
-usethis::use_data(testfile, overwrite = TRUE)
-
-
